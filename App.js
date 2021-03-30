e613965d75a94f58bbb3af3c4e708d5d547a2471
Nikita Aleksandrovich <aleksandrovich.mikita@gmail.com>
2021-03-30 17:49:45 +0300

    Add header + flat list with icons

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: '1', text: 'Milk'},
+    {id: '2', text: 'Eggs'},
+    {id: '3', text: 'Bread'},
+    {id: '4', text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello world!</Text>
-      <Image
-        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
-        style={styles.img}
+      <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item} />}
       />
     </View>
   );
@@ -16,17 +25,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2,
+    paddingTop: 60,
   },
 });
 
